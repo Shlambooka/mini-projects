b24fd49d6f12989b03eaf092b65f8cb3801a1858
Nicole Nascimento <lil.miss.abyss@gmail.com>
2024-10-23 16:48:55 -0300

    update: random number generator mini-project
    Add math for random number from 1 to 10

--- js/random-number-generator.js
+++ js/random-number-generator.js
@@ -1,11 +1,10 @@
 const numberGeneratorBtn = document.getElementById("submit")
-
-const numContainer = document.getElementById("myH1")
+const numContainer = document.getElementById("myHolder")
 
 let displayNumber = "0"
 
 numberGeneratorBtn.onclick = function() {
-    displayNumber = Math.floor(Math.random() * 100)
+    displayNumber = Math.floor(Math.random() * 10 + 1)
     // displayNumber = Math.random()
     numContainer.textContent = displayNumber
 }
