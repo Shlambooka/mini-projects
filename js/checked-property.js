d61afd370726f8b3d274a192ef5d3313c24d950f
Nicole Nascimento <lil.miss.abyss@gmail.com>
2024-10-26 08:21:16 -0300

    add:    checkbox property mini-project

---
+++ js/checked-property.js
@@ -0,0 +1,29 @@
+const myCheckbox = document.getElementById("myCheckbox")
+const visaBtn = document.getElementById("visaBtn")
+const masterCardBtn = document.getElementById("masterCardBtn")
+const payPalButton = document.getElementById("payPalButton")
+const mySubmit = document.getElementById("mySubmit")
+const mySubResult = document.getElementById("myOutput")
+const myPaymentResult = document.getElementById("myPayment")
+
+mySubmit.onclick = function() {
+    if (myCheckbox.checked){
+        mySubResult.textContent = ("You have subscribed.")
+    } 
+    else {
+        mySubResult.textContent = ("You have NOT subscribed.")
+    }
+
+    if (visaBtn.checked){
+        myPaymentResult.textContent = ("You are paying with Visa.")
+    }
+    else if (masterCardBtn.checked){
+        myPaymentResult.textContent = ("You are paying with MasterCard.")
+    }
+    else if (payPalButton.checked){
+        myPaymentResult.textContent = ("You are paying with PayPal.")
+    }
+    else {
+        myPaymentResult.textContent = ("Please select payment type.")
+    }
+}
