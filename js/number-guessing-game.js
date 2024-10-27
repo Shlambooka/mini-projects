ce59db43edb1895311847d77d9768c64f8311627
Nicole Nascimento <lil.miss.abyss@gmail.com>
2024-10-27 15:10:35 -0300

    add:    improve language with template literals

--- js/number-guessing-game.js
+++ js/number-guessing-game.js
@@ -20,22 +20,18 @@ function checkInput(){
         randomNum = Math.floor(Math.random() * 100 + 1)
     }
     else if (userInput > 100 || userInput < 1) {
-        console.log("That number isn't between 1 and 100. Try again.", typeof userInput)
         userInput = window.prompt("That number isn't between 1 and 100. Try again.")
         convertToNumber()
     } 
-    else if (userInput < randomNum) {
-        console.log("Try a higher number.", typeof userInput)    
-        userInput = window.prompt("Try a higher number.")
+    else if (userInput < randomNum) {   
+        userInput = window.prompt(`The number is higher than ${userInput}.`)
         convertToNumber()
     } 
     else if (userInput > randomNum) {
-        userInput = window.prompt("Try a lower number.")
-        console.log("Try lower.", typeof userInput, randomNum)
+        userInput = window.prompt(`The number is lower than ${userInput}`)
         convertToNumber()
     }
     else {
-        console.log("Is that even a number?! Try again.", typeof userInput)
         userInput = window.prompt("Is that even a number?! Try again.")
         convertToNumber()
     }
