c0e9acfec6db6bf44fe0b620807d379c2a3f1ee0
Nicole Nascimento <lil.miss.abyss@gmail.com>
2024-11-20 10:43:36 -0400

    redo:   tried a new approach with tutorial

--- js/dice-roller.js
+++ js/dice-roller.js
@@ -1,3 +1,30 @@
+
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value
+    const diceResult = document.getElementById("diceResult")
+    const diceImages = document.getElementById("diceImages")
+    const values = []
+    const images = []
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1
+        values.push(value)
+        images.push(`<img src="../images/dice-icons/${value}.svg" alt="Dice ${value}" width="75px">`)
+    }
+    diceResult.textContent = `Dice Result: ${values.join(', ')}`
+    diceImages.innerHTML = images.join("")
+}
+
+
+
+
+
+
+
+
+
+
+/*
 const rollBtn = document.getElementById("submit")
 const dice = document.getElementById("dice")
 const max = 6
@@ -15,10 +42,13 @@ rollBtn.onclick = function(){
     if (userInput > max || userInput < min){
         window.alert("Please choose a number between 0 and 6.")
     } 
-    else {
+    else if (userInput == 1){
         let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
         console.log(randomNum)  
         displaySide(randomNum)
+    }
+    else {
+        console.log("userInput is not equal to 1")
     }
 }
 
@@ -50,3 +80,4 @@ function displaySide(side){
             break
     }
 }
+    */
